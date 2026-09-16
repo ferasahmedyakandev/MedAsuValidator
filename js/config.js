4acc04d407efc21fee0ff3876bc83ed8ccd2b87e
ferasahmedyakandev <ferasahmedyakan@gmail.com>
2026-09-16 17:07:35 +0300

    modify

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Google Apps Script Web App URL here, ending in /exec.
-const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyNEdVlKMZ0OP4wVYugfuXXYJBbEhWVu03LTPGEnIW6zPqjzp9ZiknZczNWtjihWQrs6w/exec';
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwcIIAdsAzTzlJZYUbtPainbVArOkzTG-VBTeTgP2ULZvg3dTHtFabtWy2Ts24wX81k1A/exec';
 const APP_NAME = 'MEDASU Scanner';
